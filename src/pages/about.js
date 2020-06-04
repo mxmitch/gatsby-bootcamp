538188a5bea18b6198c459e3574aa40d600425f1
Mitch Lum <mklum88@gmail.com>
2020-06-04 13:08:08 -0700

    added about blog and contact pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This page is about the author.</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>You can reach us at (604)222-2222</p>
+    </div>
+  )
+}
+
+export default ContactPage
