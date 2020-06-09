48d7b6b3f204410dbfe2989a80e8ecb16a17f0df
Mitch Lum <mklum88@gmail.com>
2020-06-09 13:22:08 -0700

    remove link to contact page on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,6 @@ const IndexPage = () => {
       <Head title="home" />
       <h1>Hello!</h1>
       <h2>I'm Mitch, a full-stack developer in beautiful Vancouver, BC</h2>
-      <p>
-        Need a developer? <Link to="/contact">Contact Me</Link>
-      </p>
     </Layout>
   )
 }
